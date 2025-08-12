8f13b995fde6a8017598655c2adffb15b0ed3e09
CandeCorradi <corradicande@gmail.com>
2025-08-12 18:53:30 -0300

    Añadir interfaz y clase genérica para servicios CRUD
    Se ha creado la interfaz `IGenericService<T>` que define métodos asíncronos para operaciones CRUD sobre entidades genéricas. También se ha implementado la clase `GenericService<T>`, que implementa esta interfaz y proporciona la estructura básica para las operaciones, aunque los métodos aún no están completamente implementados.

---
+++ WindowsForms/Services/GenericService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsForms.DataContext;
+using WindowsForms.Interfaces;
+
+namespace WindowsForms.Services
+{
+    public class GenericService<T> : IGenericService<T> where T : class
+    {
+        public async Task<T?> AddAsync(T? entity)
+        {
+            using (CineContext cine = new CineContext())
+            {
+
+            }
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>?> GetAllAsync(string? filtro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T? entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
